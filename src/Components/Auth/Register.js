797515fb0d682478a21e46f172b951d704cab703
Shimon asulin <asulin.shimon699@gmail.com>
2022-07-07 16:41:45 +0300

    .

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -22,7 +22,7 @@ const GlobalStyle = createGlobalStyle`
 
 const Container = styled.div`
   background-color: ${primary2Color};
-    height: 100vh;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
